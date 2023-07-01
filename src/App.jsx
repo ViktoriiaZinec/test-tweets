9b6b3855def1cdf8f70799b252ed34aaaa17aeda
ViktoriiaZinec <victoria.zinec@gmail.com>
2023-07-01 22:24:12 +0300

    Run deploy

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
         <img src={reactLogo} className="logo react" alt="React logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
