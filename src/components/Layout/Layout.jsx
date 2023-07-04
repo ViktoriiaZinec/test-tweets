ebe99412d02ca98a22c0554ba7810c5e09eeec14
ViktoriiaZinec <victoria.zinec@gmail.com>
2023-07-04 13:04:08 +0300

    Add 404

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,17 @@
 import { NavLink, Outlet } from "react-router-dom";
+import css from "./Layout.module.css";
 export const Layout = () => {
   return (
     <>
       <header>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/tweets">Tweets</NavLink>
+        <nav className={css.nav}>
+          <div className={css.navbar}>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/tweets">Tweets</NavLink>
+          </div>
+        </nav>
       </header>
+
       <div>
         <Outlet />
       </div>
