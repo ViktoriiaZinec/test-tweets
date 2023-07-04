2c9bc120095f11aafdc2dcb15b5b1fc5f24ff80f
ViktoriiaZinec <victoria.zinec@gmail.com>
2023-07-04 17:42:29 +0300

    Deleted basename

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <HashRouter basename="/test-tweets">
+  <HashRouter>
     <App />
   </HashRouter>
   // </React.StrictMode>
